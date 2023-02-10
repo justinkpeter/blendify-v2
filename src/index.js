67ff5cbba500eb9ecf4c971eb5926671d4666314
Justin <justinkmpeter@gmail.com>
2023-02-10 17:00:10 -0600

    Wrapping app with data layer info

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { DataLayer } from "./utils/DataLayer";
+import reducer, {initialState} from "./utils/reducer";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+        <App />
+    </DataLayer>
   </React.StrictMode>
 );
