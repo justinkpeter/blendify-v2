c1784c7f92f073879900ebbcd00f3bd8e932418b
Justin <justinkmpeter@gmail.com>
2023-03-01 12:44:14 -0600

    A reusable component for rendering items in a grid position that accepts a column start and row start

---
+++ src/components/Tile.jsx
@@ -0,0 +1,28 @@
+export const Tile = ({col, row, children, large}) => {
+
+    const tileStyle = large ?
+        {
+            gridColumnStart: col,
+            gridColumnEnd: col + 5,
+            gridRowStart: row,
+            gridRowEnd: row + 6
+        } :
+        {
+            gridColumnStart: col,
+            gridColumnEnd: col + 3,
+            gridRowStart: row,
+            gridRowEnd: row + 3
+        }
+
+    const tileClass = 'btn w-full h-full px-0 bg-transparent border-none hover:bg-transparent hover:border-none'
+
+    return (
+        <>
+            <div className={'relative grid'} style={tileStyle}>
+                <label htmlFor={'test'} className={tileClass}>
+                    {children}
+                </label>
+            </div>
+        </>
+    );
+}
