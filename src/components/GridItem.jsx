17fa181d2814530c89390390e62c23e2a176e5dc
Justin <justinkmpeter@gmail.com>
2023-03-06 15:53:46 -0600

    Created Favorite Artists section

--- src/components/GridItem.jsx
+++ src/components/GridItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-export const GridItem = ({ col, row, children, large }) => {
+import { motion } from "framer-motion";
+export const GridItem = ({ col, row, children, large, animate, custom}) => {
 
     // set grid item style based on if large prop is passed
     const gridItemStyle = large ?
@@ -17,14 +17,36 @@ export const GridItem = ({ col, row, children, large }) => {
             gridRowEnd: row + 3
         }
 
+    const variants = {
+        visible: i => ({
+            scale: 1,
+            opacity: 1,
+            transition: {
+                delay: i * 0.6,
+                duration: 0.8,
+                type: 'easeInOut',
+            },
+            transformOrigin: 'top left'
+        }),
+        hidden: { opacity: 0, scale:0, transition: {
+            delay: 1.4,
+            },
+            transformOrigin: 'top left'},
+    }
 
-    // use inline div style to position grid items
+
+
     return (
-        <div className={'relative grid'}
-             style={gridItemStyle}
+        <motion.div
+            className={'relative grid'}
+            style={gridItemStyle}
+            initial={'hidden'}
+            animate={animate}
+            variants={variants}
+            custom={custom}
         >
             {children}
-        </div>
+        </motion.div>
     )
 };
 
---
+++ src/components/Sections/FavoriteArtists.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import {ArtistCard} from '../ArtistCard/ArtistCard';
+import { GridItem } from '../GridItem';
+
+export const FavoriteArtists = ({favoriteArtists, onUpdateData}) => {
+
+    const gridPositions = [
+        {row: 3, col: 10},
+        {row: 4, col: 15},
+        {row: 7, col: 18},
+        {row: 3, col: 19},
+        {row: 2, col: 23}
+    ]
+    const handleButtonClick = (artist) => {
+        if(artist) return 0;
+        else
+            onUpdateData(artist?.id);
+    };
+
+    if(!favoriteArtists) return null;
+
+    return (
+        <>
+            <div className={'grid grid-cols-26 grid-rows-10 h-section relative'}>
+
+                {/*    summary  */}
+                <div className={'relative grid grid-cols-7 col-[3_/_span_7] row-[4_/_span_6]  z-20'}>
+                    <div className={'absolute top-0 left-0 leading-6'}>
+                        <h1 className={'my-5 font-black text-6xl 2xl:text-7xl text-white'}>
+                        <span className={'inline-block lg:pr-[5vh]'}>
+                            <motion.span initial={{opacity: 0 ,  y:100}} animate={{opacity: 1 ,  y:0 }} transition={{duration: 0.4, delay: 0.5}}> Grooving to </motion.span><br/>
+                            <motion.span initial={{opacity: 0 ,  y:100}} animate={{opacity: 1 ,  y:0 }} transition={{duration: 0.4, delay: 0.8}} className={'break-normal text-green-400'}> {favoriteArtists[0]?.name} </motion.span>
+                            <br/>
+                         </span>
+                        </h1>
+                        <p className={'font-light text-xl text-gray-100 pr-[10vh]'}>
+                        <motion.span className={'inline-block leading-loose'} initial={{opacity: 0, y:30}} animate={{opacity: 1, y:0 }} transition={{duration: 0.8, delay: 0.8}}>
+                            When it comes to that one artist to grace your ears, nobody does is quite
+                            like {favoriteArtists[0]?.name} for you!
+                        </motion.span>
+                        </p>
+                    </div>
+                </div>
+
+                {/* artist cards */}
+                { gridPositions.map((position, index) => {
+
+                    // Top Artist is a large grid item and large artist card
+                    if(index === 0){
+                        return (
+                            <GridItem key={index} row={position.row} col={position.col} large animate={'visible'} custom={index}>
+                                <label htmlFor="my-modal-3" className="absolute btn w-full h-full border-0" onClick={handleButtonClick(favoriteArtists[index])}>
+                                    <ArtistCard large artist={favoriteArtists[index]}  />
+                                </label>
+                            </GridItem>
+                        )
+                    }
+                    return (
+                        <GridItem key={index} row={position.row} col={position.col} animate={'visible'} custom={index}>
+                            <label htmlFor="my-modal-3" className="absolute btn w-full h-full border-0" onClick={handleButtonClick(favoriteArtists[index])}>
+                                <ArtistCard rank={index + 1} artist={favoriteArtists[index]}  />
+                            </label>
+                        </GridItem>
+                    )
+                })}
+
+
+            </div>
+
+
+        </>
+    );
+}
