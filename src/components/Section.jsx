6d8fa70d841345d73fd1d06aa996ee0c74d70213
Justin <justinkmpeter@gmail.com>
2023-03-05 23:25:38 -0600

    created sections for top genres and streaming analysis

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,19 @@
-export const Section = ({topTracks, children}) => {
+export const Section = ({topTracks, topGenres, streamingAnalysis, children}) => {
 
     if(topTracks){
         return (
             <section className={'grid grid-cols-33 grid-rows-10 h-section w-[330vh]'}> {children} </section>
+        )
+    }
+
+    if(topGenres){
+        return (
+            <section className={'grid grid-cols-19 grid-rows-10 h-section w-[190vh] text-white'}> {children} </section>
+        )
+    }
+    if(streamingAnalysis){
+        return (
+            <section className={'grid grid-cols-26 grid-rows-10 h-section w-[260vh] text-white'}> {children} </section>
         )
     }
     else{
