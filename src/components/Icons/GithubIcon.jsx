e28dbca16d56f72812ba1309c4a621806aa25534
Justin <justinkmpeter@gmail.com>
2023-03-07 10:20:56 -0600

    Created GitHub Svg and Icon

---
+++ src/components/Icons/GithubIcon.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as GitHub } from '../../assets/icons/github.svg';
+
+export const GithubIcon = () => {
+    return (
+        <> <GitHub/> </>
+    );
+}
