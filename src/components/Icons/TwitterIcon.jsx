1d67f5f68638d9c373b7e57bf23b80ce2c9b1378
Justin <justinkmpeter@gmail.com>
2023-03-07 10:57:03 -0600

    Added twitter svg

---
+++ src/components/Icons/TwitterIcon.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as Twitter } from '../../assets/icons/twitter.svg';
+
+export const TwitterIcon = () => {
+    return (
+        <> <Twitter/> </>
+    );
+}
