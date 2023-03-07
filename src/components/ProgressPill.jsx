1aa09ea936dc12ed99f9bef419142aebbe6c2565
Justin <justinkmpeter@gmail.com>
2023-03-07 13:26:30 -0600

    Added "taste Profile" label to ProgressPill.jsx

--- src/components/ProgressPill.jsx
+++ src/components/ProgressPill.jsx
@@ -7,16 +7,17 @@ export const ProgressPill = () => {
 
     const [scrollerStyle, setScrollerStyle] = useState(`translate3d(0px, 0%, 0px)`);
 
-    // add scroll with spring effect
+    // framer motion progress bar
     const { scrollYProgress } = useScroll();
     const scaleX = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
     // sections
     const sections = [
         {name: "Favorite Artists", color: "bg-yellow-500"},
-        {name: "Favorite Tracks", color: "bg-blue-500"}
-        // {name: "Personality Profile", color: "bg-yellow-500"},
-        // {name: "Audio Analysis", color: "bg-red-500"}
+        {name: "Favorite Tracks", color: "bg-blue-500"},
+        {name: "Top Genres", color: "bg-red-500"},
+        {name: "Taste Profile", color: "bg-blue-500"},
+
     ]
 
     function getScrollerStyle( label ){
@@ -26,46 +27,50 @@ export const ProgressPill = () => {
             case "Favorite Artists":
                 setScrollerStyle('translate3d(0px, 0%, 0px)')
                 break
-
             case "Favorite Tracks":
-                setScrollerStyle('translate3d(0px, -45%, 0px)')
+                setScrollerStyle('translate3d(0px, -25%, 0px)')
                 break
+            case "Top Genres":
+                setScrollerStyle('translate3d(0px, -50%, 0px)')
+                break
+            case "Taste Profile":
+                setScrollerStyle('translate3d(0px, -75%, 0px)')
+                break
+
         }
     }
 
     // handles label on progress bar when scrolling through sections
     useEffect(() => {
         document.addEventListener("scroll", () => {
-            // need to find a way to dynamically get the 'breakpoints' for each section
-            // I used 33% for each section, because I have 3 sections at the time of writing this
-            // so 100 / 3 = 33
-            // but if I add more sections, I need to dynamically calculate the percentage
-            const currentSection = sections[Math.floor(getScrollPercentage() / 30)]?.name
+            const currentSection = sections[Math.floor(getScrollPercentage() /  ( 100 / sections.length ))]?.name
             getScrollerStyle(currentSection)
         });
 
-    }, [sections])
+    })
 
 
     return (
-        <div className="navbar-center justify-start rounded-full h-10 bg-red-300">
-            <div className={"flex relative bg-green-500 rounded-full h-full w-48 object-contain place-content-center z-0 overflow-hidden"}>
-                <motion.div
-                    id={"progress-bar"}
-                    style={{scaleX, transformOrigin: 'left center'}}
-                    className={"flex absolute left-0 w-full h-full bg-green-700 rounded-l-lg will-change-transform z-0"}
-                >
-                </motion.div>
-                <div className={"flex flex-col transition absolute z-10 gap-y-3 font-bold text-sm p-2 text-white ease-out duration-500"}
-                     style={{transform: `${scrollerStyle}`}}>
-                    {sections.map((section) => {
-                        return(
-                            <span key={section.name}> {section.name } </span>
-                        )
-                    })}
-
+        <>
+            <div className="navbar-center justify-start rounded-full h-10 bg-red-300">
+                <div className={"flex relative bg-green-500 rounded-full h-full w-48 object-contain place-content-center z-0 overflow-hidden"}>
+                    <motion.div
+                        id={"progress-bar"}
+                        style={{scaleX, transformOrigin: 'left center'}}
+                        className={"flex absolute left-0 w-full h-full bg-green-700 rounded-l-lg will-change-transform z-0"}
+                    >
+                    </motion.div>
+                    <div className={"flex flex-col transition absolute z-10 gap-y-2 font-bold text-sm pt-2 text-white ease-out duration-500"}
+                         style={{transform: `${scrollerStyle}`}}>
+                        {sections.map((section) => {
+                            return(
+                                <span className={'flex justify-center w-full'} key={section.name}> {section.name } </span>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+
     );
 }
