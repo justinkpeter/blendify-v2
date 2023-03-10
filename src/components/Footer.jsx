2d7896f5c279065f58e733334e487b9d90f20677
Justin <justinkmpeter@gmail.com>
2023-03-10 00:03:24 -0600

    silly me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { ScrollDirectionIcon } from "./Icons/ScrollDirectionIcon";
 export const Footer = () => {
     return (
         <>
-            <footer className="footer fixed bottom-0 z-50 items-center p-7  text-neutral-content">
+            <footer className="fixed footer   bottom-0 left-0 z-50 items-center p-7  text-neutral-content">
                 <div className="items-center grid-flow-col">
                     <ScrollDirectionIcon />
                     <span className={'font-medium text-md'}> Scroll to navigate </span>
