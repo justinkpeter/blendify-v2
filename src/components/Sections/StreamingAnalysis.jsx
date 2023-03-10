9dc8e7be604ad62064da14bdb2520fd8076d66a5
Justin <justinkmpeter@gmail.com>
2023-03-10 00:06:45 -0600

    restructure component

--- src/components/Sections/StreamingAnalysis.jsx
+++ src/components/Sections/StreamingAnalysis.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GenrePill } from "../Modal";
 import { SectionIcon } from '../SectionIcon';
 import { UserPlusIcon } from "@heroicons/react/20/solid";
 export const StreamingAnalysis = ({user, playlists, favoriteTracks, favoriteArtists, favoriteGenres }) => {
@@ -12,120 +11,75 @@ export const StreamingAnalysis = ({user, playlists, favoriteTracks, favoriteArti
 
     return (
         <>
-            <section className={'grid grid-cols-26 grid-rows-10 h-section w-[260vh] text-white'}>
+            <section className={'grid grid-cols-26 grid-rows-10 h-section text-white'}>
                 <SectionIcon Icon={UserPlusIcon} gradient={'bg-gradient-to-r from-yellow-400 to-teal-500'}/>
-
                 <div className={'relative grid grid-cols-7 col-[3_/_span_7] row-[4_/_span_4] z-20'}>
                     <div className={'absolute top-0 left-0 leading-6'}>
                         <h1 className={'my-5 font-black text-6xl 2xl:text-7xl text-white'}>
                             <span className={'inline-block xl:pr-[5vh] leading-[5rem]'}>
                                 <span> {user?.display_name.split(" ")[0] }, </span><br/>
-                                <span className={'break-normal text-green-400'}> here's your taste profile </span>
+                                <span className={'break-normal text-green-400'}> your taste profile </span>
                                 <br/>
                                 </span>
                         </h1>
                         <p className={'font-light text-xl text-gray-100 '}>
                             <span className={'inline-block leading-loose'}>
-                                All of your analytics, <span className={'italic'}> blended </span> into a cohesive profile
-                                <span className={'italic'}>Your music taste is truly immaculate. </span>
+                                All of your analytics, <span className={'italic'}> blended </span> into one.
+                                <span className={'italic'}> Your music taste is truly immaculate. </span>
                             </span>
                         </p>
                     </div>
                 </div>
-
-                {/*<StreamingAnalysis/>*/}
-                <div className={'relative grid grid-cols-13 grid-rows-6 col-[11_/_span_10] row-[3_/_span_5]  z-20  rounded-3xl  glass bg-black/30 '}>
-
-                    {/*<div className={'absolute top-0  w-full h-full glass z-0 '}/>*/}
-                    <div className={'col-[1_/_span_6] row-[1_/_span_6] pt-10 relative pl-10'}>
-                        <div className="w-56 h-56 pb-3 ">
-                            <img
-                                src={user?.images[0].url } alt="Avatar Tailwind CSS Component"
-                                className=" w-full h-full object-cover rounded-lg"/>
-                        </div>
-                        <div>
-                            <ul className={''}>
-                                <li className={'stat-value'}> { user?.display_name } </li>
-                                <li className={'stat-title'}> { user?.followers.total } followers · { playlists?.total } Public Playlists </li>
-                            </ul>
-                        </div>
-
-                        <div className={'mt-5 w-64'}>
-                            {favoriteGenres?.map((genre) => {
-                                return(
-                                    <GenrePill genre={genre.name}/>
-                                )
-                            })}
+                <div className={'relative grid grid-cols-10 grid-rows-6 col-[11_/_span_12] row-[3_/_span_7]  p-5 z-20  rounded-3xl '}>
+                    <div className={'relative col-[1_/_span_12] row-[1_/_span_2] group'}>
+                        <div className=" relative w-full h-full rounded-2xl shadow-2xl overflow-hidden">
+                            <img src={ user?.images[0].url } alt={''} className={'h-full w-full object-cover group-hover:scale-125 duration-[1200ms] transition ease-in-out'}/>
+                            <div className={'w-full h-full absolute top-0 bg-black/80'}/>
                         </div>
                     </div>
-
-                    {/* top artists*/}
-                    <div className={'col-[4_/_span_2] row-[1_/span_6] pt-10 relative '}>
-                        <div>
-                            <div className="stat-title font-black text-2xl ">Your Top Artists </div>
-                            <ul>
-                                { favoriteArtists?.slice(0,5).map((artist, index) => (
-                                    <>
-                                        <li key={index} className={'text-white/60 h-fit space-y-3 relative flex items-center gap-3   '}>
-                                            <span className={'text-2xl font-black'}>{index+1} </span>
-
-                                            <div className="w-16 h-16 ">
-                                                <img
-                                                    src={artist?.images[0].url } alt="Avatar Tailwind CSS Component"
-                                                    className=" w-full h-full object-cover "/>
-                                            </div>
-
-                                            <div>
-                                                <div className=""> { artist?.name }</div>
-                                                {/*<div className="stat-value">89,400</div>*/}
-                                            </div>
-                                        </li>
-                                        <hr className={'opacity-10 my-4'}/>
-                                    </>
-
-                                ))}
-                            </ul>
-
+                    <div className={'relative col-[4_/_span_7] row-[1_/_span_2]'}>
+                        <div className={'inline-block w-full h-full flex flex-col items-start justify-center pl-10 '}>
+                            <h1 className={'text-5xl stat-value'}> {user?.display_name} </h1>
+                            <h5 className={'stat-title mt-2'}> { user?.followers.total } followers · { playlists?.total } Public Playlists </h5>
                         </div>
                     </div>
-
-                    {/* top tracks */}
-                    <div className={'col-[7_/_span_3] row-[1_/span_6] pt-10 relative '}>
-                        <div>
-                            <div className="stat-title font-black text-2xl ">Your Top Tracks </div>
-                            <ul>
-                                { favoriteTracks.slice(0,5).map((track, index) => (
-                                    <>
-                                        <li key={index} className={'text-white/60 h-fit space-y-3 relative flex items-center gap-3   '}>
-                                            <span className={'text-2xl font-black'}>{index+1} </span>
-                                            <div className="w-16 h-16 ">
-                                                <img
-                                                    src={track?.album.images[0].url } alt="Avatar Tailwind CSS Component"
-                                                    className=" w-full h-full object-cover "/>
-                                            </div>
-                                            <div>
-                                                <div className="font-medium"> { track?.name }</div>
-                                                <div className="font-thin">
-                                                    {track?.artists?.map((artist, index) => {
-                                                        return(
-                                                            <>
-                                                                <span> {artist.name}</span>
-                                                                {index < track.artists.length - 1 && <span> , </span>}
-                                                            </>
-                                                        )
-                                                    })}
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <hr className={'opacity-10 my-4'}/>
-                                    </>
-
-                                ))}
-                            </ul>
+                    <div className={'col-[1_/_span_10] row-[3_/_span_4] flex space-x-5 pt-12 flex justify-center'}>
+                        <div className={'px-3 py-6 w-54 h-64 flex flex-col items-center bg-zinc-800/30 rounded-lg hover:bg-zinc-500/30 duration-200 transition ease-in'}>
+                            <div className="avatar">
+                                <div className="w-36 rounded-full shadow-2xl">
+                                    <img src={favoriteArtists[0]?.images[0].url } alt={''} />
+                                </div>
+                            </div>
+                            <div className={'mt-6 w-full h-fit self-start'}>
+                                <p className={'stat-title text-xs font-medium'}> Artist of the month </p>
+                                <h4 className={'text-md font-bold'}> {favoriteArtists[0]?.name }</h4>
+                            </div>
+                        </div>
+                        <div className={'px-3 py-6 w-54 h-64 flex flex-col items-center bg-zinc-800/30 rounded-lg hover:bg-zinc-500/30 duration-200 transition ease-in'}>
+                            <div className="avatar">
+                                <div className="w-36 rounded-full shadow-2xl">
+                                    <img src={favoriteTracks[0]?.album.images[0].url } alt={''}/>
+                                </div>
+                            </div>
+                            <div className={'mt-6 w-full h-fit self-start overflow-hidden'}>
+                                <p className={'stat-title text-xs font-medium'}> Song of the month </p>
+                                <h4 className={'text-md font-bold'}> {favoriteTracks[0]?.name }</h4>
+                            </div>
+                        </div>
+                        <div className={'px-3 py-6 w-54 h-64 flex flex-col items-center bg-zinc-800/30 rounded-lg hover:bg-zinc-500/30 duration-200 transition ease-in'}>
+                            <div className="avatar">
+                                <div className="relative w-36 rounded-full shadow-2xl bg-white/5">
+                                    <span className={'absolute inset-0 bg-gradient-to-r from-fuchsia-500 via-red-300 to-orange-400 h-full flex items-center justify-center bg-clip-text text-transparent text-sm font-bold'}>
+                                        {favoriteGenres[0]?.name}.
+                                    </span>
+                                </div>
+                            </div>
+                            <div className={'mt-6 w-full h-fit self-start'}>
+                                <p className={'stat-title text-xs font-medium'}> Genre of the month </p>
+                                <h4 className={'text-md font-bold'}> {favoriteGenres[0]?.name }</h4>
+                            </div>
                         </div>
                     </div>
-
-
                 </div>
             </section>
         </>
